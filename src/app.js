44887acfe7398f05248aa6da0565e29ea82da7f2
JTLiner925 <[jtliner925@gmail.com]>
2020-05-29 14:16:07 -0500

    called note and folder using router and service

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ const morgan = require("morgan");
 const cors = require("cors");
 const helmet = require("helmet");
 const { NODE_ENV } = require("./config");
+const notesRouter = require('./notes/notes-router');
+const foldersRouter = require('./folders/folders-router');
 
 const app = express();
 
@@ -12,6 +14,9 @@ app.use(morgan((NODE_ENV === 'production') ? 'tiny' : 'common', {
 }))
 app.use(helmet());
 app.use(cors());
+
+app.use('/api/folder', foldersRouter)
+app.use('/api/note', notesRouter)
 
 app.get("/", (req, res) => {
   res.send("Hello, world!");
