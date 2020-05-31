d933ed837199187e469fa4719ade28ed46838f79
JTLiner925 <[jtliner925@gmail.com]>
2020-05-31 07:33:53 -0500

    working on folder_id

--- src/folders/folders-router.js
+++ src/folders/folders-router.js
@@ -36,4 +36,21 @@ foldersRouter
       })
       .catch(next);
 })
+foldersRouter.route('/:folder_id')
+.all((req, res, next) => {
+  FoldersService.getById(req.app.get('db'), req.params.folder_id)
+  .then((folder) => {
+    if (!folder){
+      return res.status(404).json({
+        error: { message: `Folder doesn't exist` },
+      });
+    }
+    res.folder = folder;
+    next();
+  })
+  .catch(next);
+})
+// .get((req, res) => {
+//   res.json(serializeFolder(res.folder));
+// })
 module.exports = foldersRouter
